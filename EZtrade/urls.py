43e39b7b65bb86819c841567881c28975dd0af97
marioguy08 <leonbrkanac@gmail.com>
2020-07-25 19:00:36 -0700

    final????

--- EZtrade/urls.py
+++ EZtrade/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include,re_path
 from django.conf.urls.static import static
 from django.conf import settings
-from djano.views.generic import TemplateView
+from django.views.generic import TemplateView
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
